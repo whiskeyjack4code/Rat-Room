d6b8319d6748214972844c7ebda8f50194ea5f59
whiskeyjack4code <davidmcmhn@gmail.com>
2026-04-13 18:03:19 +0100

    Allow broadcasting so when a client sends a message the server will echo it back on a cloned tcpstream

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,5 +1,6 @@
 use std::io::Write;
 use std::net::TcpStream;
+use std::io::Read;
 
 fn main(){
     let mut stream = TcpStream::connect("127.0.0.1:8888").unwrap();
@@ -9,4 +10,13 @@ fn main(){
     stream.write_all(message.as_bytes()).unwrap();
 
     println!("Message sent");
+
+    let mut buffer = [0; 1024];
+    let bytes_read = stream.read(&mut buffer).unwrap();
+
+    println!(
+        "Broadcast received: {}",
+        String::from_utf8_lossy(&buffer[..bytes_read])
+    );
+
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,26 +1,33 @@
 use std::net::{TcpListener, TcpStream};
-use std::io::Read;
+use std::io::{Read, Write};
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::{Arc, Mutex};
 use std::thread;
 
 const SOCKET: &str = "127.0.0.1:8888";
 
-fn handle_client(mut stream: TcpStream, id: usize, clients: Arc<Mutex<Vec<usize>>>) {
+fn handle_client(mut stream: TcpStream, id: usize, clients: Arc<Mutex<Vec<TcpStream>>>) {
 
     {
         let mut clients_list = clients.lock().unwrap();
-        clients_list.push(id);
-        println!("Connected clients: {:?}", *clients_list);
+        let cloned_stream = stream.try_clone().unwrap();
+        clients_list.push(cloned_stream);
+        println!("Client {id} added. Total clients: {}", clients_list.len());
     }
 
     let mut buffer = [0; 1024];
     let bytes_read = stream.read(&mut buffer).unwrap();
 
-    println!(
-        "Client {id} says: {}",
-        String::from_utf8_lossy(&buffer[..bytes_read])
-    );
+    let message = String::from_utf8_lossy(&buffer[..bytes_read]);
+    println!("Client {id} says: {}", message);
+
+    {
+        let mut clients_list = clients.lock().unwrap();
+
+        for client_stream in clients_list.iter_mut() {
+            client_stream.write_all(message.as_bytes()).unwrap();
+        }
+    }
 }
 
 fn main() {
