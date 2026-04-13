f706b9d8e59447f58156bcf0a677192bd63f0eca
whiskeyjack4code <davidmcmhn@gmail.com>
2026-04-13 17:40:17 +0100

    Keep the main server free to accept new client connections while a worker handles each dedicated connection

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,22 +1,30 @@
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 use std::io::Read;
+use std::thread;
 
 const SOCKET: &str = "127.0.0.1:8888";
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    let bytes_read = stream.read(&mut buffer).unwrap();
+
+    println!(
+        "Message: {}",
+        String::from_utf8_lossy(&buffer[..bytes_read])
+    );
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8888").unwrap();
     println!("Server listening on {SOCKET}");
 
     loop {
-        let (mut stream, addr) = listener.accept().unwrap();
+        let (stream, addr) = listener.accept().unwrap();
         println!("Client connected from {addr}");
 
-        let mut buffer = [0; 1024];
-        let bytes_read = stream.read(&mut buffer).unwrap();
-
-        println!("Received {bytes_read} bytes");
-        println!("Message: {}", String::from_utf8_lossy(&buffer[..bytes_read]));
+       thread::spawn(move || {
+           handle_client(stream)
+       });
 
     }
-
 }
